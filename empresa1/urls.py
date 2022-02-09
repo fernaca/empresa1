0b8f24814249ec0b8b74126decd84e9db387e04d
Fernando <fernando.caceres@gmail.com>
2022-02-09 11:56:33 -0300

    Upload de imagenes

--- empresa1/urls.py
+++ empresa1/urls.py
@@ -15,13 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
- 
+# para Media
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(''               , include('website.urls'), name="home"),
+    path('', include('website.urls'), name="home"),
     # path('single-product/', include('website.urls'), name="product"),
     # path('about/'         , include('website.urls'), name="about"),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 #handler404 = "website.views.page_not_found_view"
